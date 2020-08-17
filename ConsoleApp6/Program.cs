fbaa7095eab5559c302896938fe53da4daa02f75
alon <alon@eshkol-it.co.il>
2020-08-17 15:30:11 +0300

    sasdsafd

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -13,7 +13,7 @@ namespace ConsoleApp6
             // Create the file, or overwrite if the file exists.
             using (FileStream fs = File.Create(path, 1024))
             {
-                byte[] info = new UTF8Encoding(true).GetBytes("This is some text sin the file!!!");
+                byte[] info = new UTF8Encoding(true).GetBytes("This is some text sin the file!!");
                 // Add some information to the file.
                 fs.Write(info, 0, info.Length);
             }
