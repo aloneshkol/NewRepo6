a9eb311617eef5d8743585463164af8eea703eec
alon <alon@eshkol-it.co.il>
2020-08-17 14:50:24 +0300

    Second commit

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace ConsoleApp6
 {
@@ -6,7 +8,25 @@ namespace ConsoleApp6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"c:\temp\MyTest.txt";
+
+            // Create the file, or overwrite if the file exists.
+            using (FileStream fs = File.Create(path, 1024))
+            {
+                byte[] info = new UTF8Encoding(true).GetBytes("This is some text sin the file.");
+                // Add some information to the file.
+                fs.Write(info, 0, info.Length);
+            }
+
+            // Open the stream and read it back.
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
         }
     }
 }
